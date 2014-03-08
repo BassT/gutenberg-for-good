1cd9cdf0c01f50b77c68e227bf637575e955ec3a
BassT <sebastian.richter@student.kit.edu>
2014-03-07 22:54:45 -0500

    added character generation controller and text analysis services, next step: pull texts from gutenberg and directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -70,37 +70,279 @@ angular.module('gutenberg.controllers', ['ui.bootstrap'])
 
   }])
 
+	/* First-oder monkey controller
+			- Generates characters from a first-order monkey
+			- Takes an array which represents the first-order analysis
+				of a text from project gutenber as input for the monkey */
+	.controller("First-orderMonkey", ["$scope", "$interval", "Typing", "myFunctions", "AnalysisFactory", "Alphabet", function($scope, $interval, Typing, myFunctions, AnalysisFactory, Alphabet) {
 
+				$scope.text = ""; //initialize emtpy text
+
+		  	var timer = {};
+		  	var word = "";
+		  	var character = "";
+		  	var matrix = AnalysisFactory.firstOrderMatrix;
+
+				$scope.$on("typingChangedBroadcast", function() { // receive event that someone clicked a typing control button
+					console.log("Received typingChangedBroadcast");
+					console.log(Typing);
+
+					if(Typing.start){ // monkey, work!
+						if(!angular.isDefined(timer)) {
+							timer = $interval( function() {
+								
+								/* ================================
+										Start character generation - the tricky party - >>>> can we move this to myFunctions? <<<<
+									 ================================ */
+
+								for (var i = matrix.length - 1; i >= 0; i--) {
+									freqTotal += matrix[1];
+								};
+
+								// generate a random number between 0 and the total of frequencies
+								var rand = Math.floor(Math.random()*freqTotal);
+
+								// find the next char: sum the frequencies until they exceed rand and take that character
+								var indexNextChar = 0;
+								var freqSum = matrix[1][0];
+								while(freqSum < rand){
+								  indexNextChar++;
+								  freqSum += matrix[1][indexNextChar];
+								}
+
+								if(matrix[0][indexNextChar] !== undefined) {
+								  var character = matrix[0][indexNextChar].slice(2); // take the last character of the trio as the next character
+								} else {
+								  // if he somehow tpyed himself to a dead end, just start over
+								  var character = matrix[0][Math.floor(Math.random()*matrix[0].length)].slice(0,2); 
+								}
+
+								/* ================================
+										End character generation
+									 ================================ */
+
+								$scope.text += character; // add actual next character to text
+								myFunctions.checkForWord(character, "straightforward");
+								$scope.$emit("TypedCharacterEmit");
+								}, Typing.speed);	
+						}
+					} else { // monkey, stop!
+						if(angular.isDefined(timer)) {
+							$interval.cancel(timer);
+							timer = undefined;
+						}
+					}
+				});
+
+	}])
+	
+	/* Second-order monkey controller
+			- Generates characters from a Second-order monkey
+			- Takes an array which represents the Second-order analysis of
+				a text from project gutenberg as input for the monkey */
+	.controller("Second-orderMonkey", ["$scope", "$interval", "Typing", "myFunctions", "AnalysisFactory", "Alphabet", function($scope, $interval, Typing, myFunctions, AnalysisFactory, Alphabet) {
+
+		$scope.text = ""; //initialize emtpy text
+
+  	var timer = {};
+  	var word = "";
+  	var character = "";
+  	var previousTypedChars = "";
+  	var matrix = AnalysisFactory.seoondOrderMatrix;
+
+		$scope.$on("typingChangedBroadcast", function() { // receive event that someone clicked a typing control button
+			console.log("Received typingChangedBroadcast");
+			console.log(Typing);
+
+			if(Typing.start){ // monkey, work!
+				if(!angular.isDefined(timer)) {
+					timer = $interval( function() {
+						
+						/* ================================
+								Start character generation - the tricky party - >>>> can we move this to myFunctions? <<<<
+							 ================================ */
+
+						previousTypedChars = $scope.text.charAt(length - 1); // get the last two typed characters
+
+						// set up slice matrix
+						matrixSlice = new Array(2);
+						matrixSlice[0] = new Array(Alphabet.length);
+						matrixSlice[1] = new Array(Alphabet.length);
+						for (var i = matrixSlice[0].length - 1; i >= 0; i--) {
+						  matrixSlice[0][i] = undefined;
+						  matrixSlice[1][i] = undefined;
+						};
+
+						// slice the to the part of third-order characters that start with the previous two typed characters
+						insertAt = 0;
+						freqTotal = 0;
+						for (var i = matrix[0].length - 1; i >= 0; i--) {
+						  if(previousTypedChars === matrix[0][i].substring(0,1)){ // check if current char matrix element starts with the correct chars
+								insertAt                 = matrixSlice[0].indexOf(undefined); // find spot to insert this item
+								matrixSlice[0][insertAt] = matrix[0][i]; // copy third-order char
+								matrixSlice[1][insertAt] = matrix[1][i]; // copy frequencey
+								freqTotal                += matrix[1][i]; // increase sum of frequencies
+						  }
+						};
+
+						// generate a random number between 0 and the total of frequencies
+						var rand = Math.floor(Math.random()*freqTotal);
+
+						// find the next char: sum the frequencies until they exceed rand and take that character
+						var indexNextChar = 0;
+						var freqSum = matrixSlice[1][0];
+						while(freqSum < rand){
+						  indexNextChar++;
+						  freqSum += matrixSlice[1][indexNextChar];
+						}
+
+						if(matrixSlice[0][indexNextChar] !== undefined) {
+						  var character = matrixSlice[0][indexNextChar].slice(1); // take the last character of the trio as the next character
+						} else {
+						  // if he somehow tpyed himself to a dead end, just start over
+						  var character = matrix[0][Math.floor(Math.random()*matrix[0].length)].slice(0,1); 
+						}
+
+						/* ================================
+								End character generation
+							 ================================ */
+
+						$scope.text += character; // add actual next character to text
+						myFunctions.checkForWord(character, "straightforward");
+						$scope.$emit("TypedCharacterEmit");
+						}, Typing.speed);	
+				}
+			} else { // monkey, stop!
+				if(angular.isDefined(timer)) {
+					$interval.cancel(timer);
+					timer = undefined;
+				}
+			}
+		});
+
+	}])
+
+	/* Third-order monkey controller
+			- Generates characters from a third-order monkey
+			- Takes an array which represents the third-order analysis of
+				a text from project gutenberg as input for the monkey */
+	.controller("Third-orderMonkey", ["$scope", "$interval", "Typing", "myFunctions", "AnalysisFactory", "Alphabet", function($scope, $interval, Typing, myFunctions, AnalysisFactory, Alphabet) {
+
+		$scope.text = ""; //initialize emtpy text
+
+  	var timer = {};
+  	var word = "";
+  	var character = "";
+  	var twoPreviousTypedChars = "";
+  	var matrix = AnalysisFactory.thirdOrderMatrix;
+
+		$scope.$on("typingChangedBroadcast", function() { // receive event that someone clicked a typing control button
+			console.log("Received typingChangedBroadcast");
+			console.log(Typing);
+
+			if(Typing.start){ // monkey, work!
+				if(!angular.isDefined(timer)) {
+					timer = $interval( function() {
+						
+						/* ================================
+								Start character generation - the tricky party - >>>> can we move this to myFunctions? <<<<
+							 ================================ */
+
+						twoPreviousTypedChars = $scope.text.slice(-2); // get the last two typed characters
+
+						// set up slice matrix
+						matrixSlice = new Array(2);
+						matrixSlice[0] = new Array(Alphabet.length);
+						matrixSlice[1] = new Array(Alphabet.length);
+						for (var i = matrixSlice[0].length - 1; i >= 0; i--) {
+						  matrixSlice[0][i] = undefined;
+						  matrixSlice[1][i] = undefined;
+						};
+
+						// slice the to the part of third-order characters that start with the previous two typed characters
+						insertAt = 0;
+						freqTotal = 0;
+						for (var i = matrix[0].length - 1; i >= 0; i--) {
+						  if(twoPreviousTypedChars === matrix[0][i].substring(0,2)){ // check if current char matrix element starts with the correct chars
+								insertAt                 = matrixSlice[0].indexOf(undefined); // find spot to insert this item
+								matrixSlice[0][insertAt] = matrix[0][i]; // copy third-order char
+								matrixSlice[1][insertAt] = matrix[1][i]; // copy frequencey
+								freqTotal                += matrix[1][i]; // increase sum of frequencies
+						  }
+						};
+
+						// generate a random number between 0 and the total of frequencies
+						var rand = Math.floor(Math.random()*freqTotal);
+
+						// find the next char: sum the frequencies until they exceed rand and take that character
+						var indexNextChar = 0;
+						var freqSum = matrixSlice[1][0];
+						while(freqSum < rand){
+						  indexNextChar++;
+						  freqSum += matrixSlice[1][indexNextChar];
+						}
+
+						if(matrixSlice[0][indexNextChar] !== undefined) {
+						  var character = matrixSlice[0][indexNextChar].slice(2); // take the last character of the trio as the next character
+						} else {
+						  // if he somehow tpyed himself to a dead end, just start over
+						  var character = matrix[0][Math.floor(Math.random()*matrix[0].length)].slice(0,2); 
+						}
+
+						/* ================================
+								End character generation
+							 ================================ */
+
+						$scope.text += character; // add actual next character to text
+						myFunctions.checkForWord(character, "straightforward");
+						$scope.$emit("TypedCharacterEmit");
+						}, Typing.speed);	
+				}
+			} else { // monkey, stop!
+				if(angular.isDefined(timer)) {
+					$interval.cancel(timer);
+					timer = undefined;
+				}
+			}
+		});
+
+	}])
+
+	/* Evaluation controller
+			- controls the evaluation panel of the monkey problems */
 	.controller("EvaluationCtrl", ["$scope", "Monkeys", function($scope, Monkeys) {
 
 		$scope.monkeys = Monkeys.monkeys;
 
-		$scope.$watch("Monkeys.monkeys[0].fakeWords", function(newVal, oldVal, scope) {
-			if(newVal !== undefined){
-				$scope.monkeys[0].fakeWords = newVal;
-				// $scope.monkeys[0].ratio = ($scope.monkeys[0].actualWords) / ($scope.monkeys[0].actualWords + $scope.monkeys[0].fakeWords);
-			}
-		});
+		/* =============
+				Personal note: This actually works! I didn't expect that.
+				Apparently $scope.$watch acts like an observer: Once it is
+				"attached" to some varible it will be triggered whenever the
+				expression is met. Cool stuff.
+		   ============= */
 
-		$scope.$watch("Monkeys.monkeys[0].actualWords", function(newVal, oldVal, scope) {
-			if(newVal !== undefined){
-				$scope.monkeys[0].actualWords = newVal;
-				// $scope.monkeys[0].ratio = ($scope.monkeys[0].actualWords) / ($scope.monkeys[0].actualWords + $scope.monkeys[0].fakeWords);
-			}
-		});
+		for (var i = Monkeys.monkeys.length - 1; i >= 0; i--) {
+			$scope.$watch("Monkeys.monkeys[" + i + "].fakeWords", function(newVal, oldVal, scope) {
+				if(newVal !== undefined) {
+					$scope.monkeys[i].fakeWords = newVal;
+				}
+			});
+			$scope.$watch("Monkeys.monkeys[" + i + "].actualWords", function(newVal, oldVal, scope) {
+				if(newVal !== undefined) {
+					$scope.monkeys[i].actualWords = newVal;
+				}
+			});
+		};
 
-		$scope.$watch("Monkeys.monkeys[1].fakeWords", function(newVal, oldVal, scope) {
-			if(newVal !== undefined){
-				$scope.monkeys[1].fakeWords = newVal;
-				// $scope.monkeys[1].ratio = ($scope.monkeys[1].actualWords) / ($scope.monkeys[1].actualWords + $scope.monkeys[1].fakeWords);
-			}
-		});
+	}])
 
-		$scope.$watch("Monkeys.monkeys[1].actualWords", function(newVal, oldVal, scope) {
-			if(newVal !== undefined){
-				$scope.monkeys[1].actualWords = newVal;
-				// $scope.monkeys[1].ratio = ($scope.monkeys[1].actualWords) / ($scope.monkeys[1].actualWords + $scope.monkeys[1].fakeWords);
-			}
-		});
+	/* Text analyzer control
+			- this controller basically just triggers the
+				text analysis of some factory */
+	.controller("TextAnalyzerCtrl", ["$scope", "GutenbergTextFactory", "AnalysisFactory", function($scope, GutenbergTextFactory, AnalysisFactory) {
+
+		$scope.analyzeText = function(order) {
+			AnalysisFactory.analyzeText(GutenbergTextFactory.text, order);
+		};
 
 	}]);
--- app/js/services.js
+++ app/js/services.js
@@ -100,6 +100,108 @@ angular.module('gutenberg.services', [])
 	})
 
 
+	.factory("AnalysisFactory", ["Alphabet", function(Alphabet) {
+
+		var AnalysisFactory = {};
+
+		AnalysisFactory.analyzeText = function(text, order) {
+			if (order === 1) {
+
+				if(AnalysisFactory.firstOrderMatrix === undefined) {
+					AnalysisFactory.firstOrderMatrix = new Array(2);
+					AnalysisFactory.firstOrderMatrix[0] = new Array(Alphabet.length);
+					AnalysisFactory.firstOrderMatrix[1] = new Array(Alphabet.length);
+					for (var i = AnalysisFactory.firstOrderMatrix[0] - 1; i >= 0; i--) {
+						AnalysisFactory.firstOrderMatrix[0] = undefined;
+						AnalysisFactory.firstOrderMatrix[1] = undefined;
+					};
+				}
+
+				for (var i = text.length - 1; i >= 0; i--) {
+					if(Alphabet.indexOf(text[i]) != -1) { // check if characters are in our alphabet
+		        c = text[i];
+		        index = AnalysisFactory.firstOrderMatrix[0].indexOf(c);
+		        if(index !== -1){ // this second-order character is already in the array
+		          AnalysisFactory.firstOrderMatrix[1][index]++; // increase frequency of this second-order character
+		        } else { // this second-order character isn't in our array yet
+		          insertAt = AnalysisFactory.firstOrderMatrix[0].indexOf(undefined); // find first empty cell
+		          AnalysisFactory.firstOrderMatrix[0][insertAt] = c; // insert second-order character
+		          AnalysisFactory.firstOrderMatrix[1][insertAt] = 1; // set frequency to 1 for this second-order character
+		        }
+	      	}
+				}
+
+				// remove unnecassary "undefined" columns
+				AnalysisFactory.firstOrderMatrix[0] = AnalysisFactory.firstOrderMatrix[0].slice(0, insertAt + 1);
+				AnalysisFactory.firstOrderMatrix[1] = AnalysisFactory.firstOrderMatrix[1].slice(0, insertAt + 1);
+
+			} else if (order === 2) {
+
+				if(AnalysisFactory.secondOrderMatrix === undefined) {
+					AnalysisFactory.secondOrderMatrix = new Array(2);
+					AnalysisFactory.secondOrderMatrix[0] = new Array(Math.pow(Alphabet.length,2));
+					AnalysisFactory.secondOrderMatrix[1] = new Array(Math.pow(Alphabet.length,2));
+					for (var i = AnalysisFactory.secondOrderMatrix[0] - 1; i >= 0; i--) {
+						AnalysisFactory.secondOrderMatrix[0][i] = undefined;
+						AnalysisFactory.secondOrderMatrix[1][i] = undefined;
+					};
+				}
+
+				for (var i = text.length - 1; i >= 0; i--) {
+					if(Alphabet.indexOf(text[i]) != -1 && Alphabet.indexOf(text[i-1]) != -1) { // check if characters are in our alphabet
+		        c = text[i-1] + text[i];
+		        index = AnalysisFactory.secondOrderMatrix[0].indexOf(c);
+		        if(index !== -1){ // this second-order character is already in the array
+		          AnalysisFactory.secondOrderMatrix[1][index]++; // increase frequency of this second-order character
+		        } else { // this second-order character isn't in our array yet
+		          insertAt = AnalysisFactory.secondOrderMatrix[0].indexOf(undefined); // find first empty cell
+		          AnalysisFactory.secondOrderMatrix[0][insertAt] = c; // insert second-order character
+		          AnalysisFactory.secondOrderMatrix[1][insertAt] = 1; // set frequency to 1 for this second-order character
+		        }
+	      	}
+				}
+
+				// remove unnecassary "undefined" columns
+				AnalysisFactory.secondOrderMatrix[0] = AnalysisFactory.secondOrderMatrix[0].slice(0, insertAt + 1);
+				AnalysisFactory.secondOrderMatrix[1] = AnalysisFactory.secondOrderMatrix[1].slice(0, insertAt + 1);
+
+			} else if (order === 3) {
+
+				if(AnalysisFactory.thirdOrderMatrix === undefined) {
+					AnalysisFactory.thirdOrderMatrix = new Array(2);
+					AnalysisFactory.thirdOrderMatrix[0] = new Array(Math.pow(Alphabet.length,3));
+					AnalysisFactory.thirdOrderMatrix[1] = new Array(Math.pow(Alphabet.length,3));
+					for (var i = AnalysisFactory.thirdOrderMatrix[0] - 1; i >= 0; i--) {
+						AnalysisFactory.thirdOrderMatrix[0][i] = undefined;
+						AnalysisFactory.thirdOrderMatrix[1][i] = undefined;
+					};
+				}
+
+				for (var i = text.length - 1; i >= 0; i--) {
+					if(Alphabet.indexOf(text[i]) != -1 && Alphabet.indexOf(text[i-1]) != -1 && Alphabet.indexOf(text[i-2]) != -1) { // check if characters are in our alphabet
+		        c = text[i-2] + text[i-1] + text[i];
+		        index = AnalysisFactory.thirdOrderMatrix[0].indexOf(c);
+		        if(index !== -1){ // this second-order character is already in the array
+		          AnalysisFactory.thirdOrderMatrix[1][index]++; // increase frequency of this second-order character
+		        } else { // this second-order character isn't in our array yet
+		          insertAt = AnalysisFactory.thirdOrderMatrix[0].indexOf(undefined); // find first empty cell
+		          AnalysisFactory.thirdOrderMatrix[0][insertAt] = c; // insert second-order character
+		          AnalysisFactory.thirdOrderMatrix[1][insertAt] = 1; // set frequency to 1 for this second-order character
+		        }
+	      	}
+				}
+
+				// remove unnecassary "undefined" columns
+				AnalysisFactory.thirdOrderMatrix[0] = AnalysisFactory.thirdOrderMatrix[0].slice(0, insertAt + 1);
+				AnalysisFactory.thirdOrderMatrix[1] = AnalysisFactory.thirdOrderMatrix[1].slice(0, insertAt + 1);
+			}
+			
+		};
+
+		return AnalysisFactory;
+
+	}]);
+
 	/* myFunctions Factory
 			This factory contains custom functions, which 
 			can be used in different controllers */
