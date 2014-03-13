4945160d39e9b045d38e8273f0a1c0c2e1c1e4ec
BassT <sebastian.richter@student.kit.edu>
2014-03-13 14:46:59 -0400

    finished and tested controller for computing correlation matrices

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -391,8 +391,6 @@ angular.module('gutenberg.controllers', ['ui.bootstrap'])
 	*/
 	.controller('CorrMatrixCtrl', ['AnalysisFactory', '$scope', function(AnalysisFactory, $scope){
 
-
-
 		$scope.characters = [];
 
 		$scope.get = function(query) {
@@ -400,12 +398,21 @@ angular.module('gutenberg.controllers', ['ui.bootstrap'])
 			var tempFreqs = AnalysisFactory.getFreqs(query.length + 1);
 			var totalFreq = 0;
 			var result = [];
-			for (var i = tempFreqs.length - 1; i >= 0; i--) {
-				totalFreq += tempFreqs[i];
-			};
-			for (var i = tempFreqs.length - 1; i >= 0; i--) {
-				result.push({ name: tempNames[i], freq: (tempFreqs[i]/totalFreq + 0.0)});
-			};
+			if(tempFreqs !== undefined && tempNames !== undefined) {
+				for (var i = tempFreqs.length - 1; i >= 0; i--) { // sum total frequencies to calculate relative probabilities
+					if(tempNames[i].substring(0, query.length) === query) { // filter to characters, which start with query
+						totalFreq += tempFreqs[i];	
+					}
+					if(totalFreq === 0) {
+						totalFreq = 1; // all of the filtered characters have 0 frequency, so set totalFreq to 1 in order to avoid dividing by 0
+					} 
+				};
+				for (var i = 0; i < tempFreqs.length; i++) {
+					if(tempNames[i].substring(0, query.length) === query) {
+						result.push({ name: tempNames[i], prob: (tempFreqs[i]/totalFreq + 0.0)});
+					}
+				};
+			}
 			return result;
 		};
 
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -18,22 +18,68 @@ describe("corrMatrixCtrl test", function() {
 			} else if (order === 2) {
 				return [1,2,3,4];
 			} else if (order === 3) {
-				return [1,2];
+				return [1,0];
 			}	
 		}
 	};
 
-	beforeEach(function() {
-        module('gutenberg.controllers');
-    });
+	var scope;
+	var ctrl;
+
+	beforeEach(module('gutenberg.controllers'))
+
+	beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+		ctrl = $controller("CorrMatrixCtrl", {AnalysisFactory: fakeFactory, $scope: scope});
+    }));
+
+	it('should have prob 0 if get("ba") is called', function() {
+		var characters = scope.get("ba");
+		expect(characters.length).toBe(1);
+		expect(characters[0].prob).toBe(0);
+	})
 
 	it("should have a get method", inject(function($controller, $rootScope) {
-
-        var scope = $rootScope.$new();
-		var ctrl = $controller("CorrMatrixCtrl", {AnalysisFactory: fakeFactory, $scope: scope});
 
 		expect(angular.isFunction(scope.get)).toBe(true);
         
 	}));
+
+	it("should return a,b,c when get('') is called", function() {
+		expect(scope.get('')[0].name).toBe("a");
+		expect(scope.get('')[1].name).toBe("b");
+		expect(scope.get('')[2].name).toBe("c");
+	});	
+
+	it("should return probabilities [1/6, 2/6, 3/6] when get('') is called", function() {
+		expect(scope.get('')[0].prob).toBe(1/6);
+		expect(scope.get('')[1].prob).toBe(2/6);
+		expect(scope.get('')[2].prob).toBe(3/6);
+	});	
+
+	it('should return ["aa", "ab", "ac", "bc"] (length 4) when get("a") is called', function() {
+		var characters = scope.get("a");
+		expect(characters[0].name).toBe("aa");
+		expect(characters[1].name).toBe("ab");
+		expect(characters[2].name).toBe("ac");
+	});
+
+	it('should have probabilities [1/6, 2/6, 3/6] when get("a") is called', function() {
+		var characters = scope.get("a");
+		expect(characters[0].prob).toBe(1/6);
+		expect(characters[1].prob).toBe(2/6);
+		expect(characters[2].prob).toBe(3/6);
+	});
+
+	it('should have prob 1 when get("b") is called', function() {
+		var characters = scope.get("b");
+		expect(characters.length).toBe(1);
+		expect(characters[0].prob).toBe(1);
+	})
+
+	it('should return [] when get("abcads") is called', function() {
+		var characters = scope.get("abcads");
+		expect(characters.length).toBe(0);
+	});
 
 });
